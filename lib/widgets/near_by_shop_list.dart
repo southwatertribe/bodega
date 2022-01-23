688a166b117311b1248cf2186ad8f0ac654e375c
Danny Carter <imdannycarter@gmail.com>
2022-01-23 16:11:06 +0000

    Converted to stateless

--- lib/widgets/near_by_shop_list.dart
+++ lib/widgets/near_by_shop_list.dart
@@ -1,18 +1,14 @@
 //This is the widget that lists the returned shop titles
 //This should be recieving from the nearby item shop
 
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:tester/widgets/item_nearbyshop.dart';
 import '../DUMMDATA.dart';
 
-class NearbyShopsList extends StatefulWidget {
+class NearbyShopsList extends StatelessWidget {
   const NearbyShopsList({ Key? key }) : super(key: key);
-
-  @override
-  _NearbyShopsListState createState() => _NearbyShopsListState();
-}
-
-class _NearbyShopsListState extends State<NearbyShopsList> {
 
   @override
   Widget build(BuildContext context) {
