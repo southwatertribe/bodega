7c9395ce4f504d4e12c8b28730d755561157a073
Danny Carter <imdannycarter@gmail.com>
2022-02-02 14:04:01 +0000

    made form to add shop and store form data TODO: Send user data to connected database

--- lib/screens/create_a_shop.dart
+++ lib/screens/create_a_shop.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_field, prefer_final_fields
+
 import 'package:flutter/material.dart';
+import 'package:tester/models/shop.dart';
 
 class CreateAShopScreen extends StatefulWidget {
   const CreateAShopScreen({ Key? key }) : super(key: key);
@@ -9,11 +12,60 @@ class CreateAShopScreen extends StatefulWidget {
 
 class _CreateAShopScreenState extends State<CreateAShopScreen> {
   
+  final _formInfo = GlobalKey<FormState>();
+  var _editedShop = Shop(
+    id: '', 
+    name: '', 
+    location: '', 
+    products: []
+    );
+
+  void _submitShop() {
+    
+    _formInfo.currentState!.save();
+    //test
+    print(_editedShop.name);
+    print(_editedShop.location);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Build-A-Shop'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formInfo,
+          child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Shop Name'),
+                textInputAction: TextInputAction.next,
+                onSaved: (val) {
+                  if (val != null) {
+                    _editedShop = Shop(name: val, id: _editedShop.id, location: _editedShop.location, products: _editedShop.products);
+                  }
+                }
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Address'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  _submitShop();
+                },
+                onSaved: (val) {
+                  if (val != null) {
+                    _editedShop = Shop(name: _editedShop.name, id: _editedShop.id, location: val, products: _editedShop.products);
+                  }
+                },
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
